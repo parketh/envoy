dbcb6a90ffb796f184d74c5dc84bfbe115512cb4
yeungparkhay <yeungparkhay@gmail.com>
2022-10-08 09:06:59 +0100

    Added chrome-aws-lambda

--- pages/api/proposal/fetch/makerdao.ts
+++ pages/api/proposal/fetch/makerdao.ts
@@ -1,68 +1,83 @@
 import { Status } from "@prisma/client"
 import { NextApiRequest, NextApiResponse } from "next"
-import { launchChromium } from "playwright-aws-lambda"
+
+import puppeteer from "puppeteer-core"
+import chrome from "chrome-aws-lambda"
 
 enum ProposalType {
     poll,
     executive,
 }
 
-const scrape = async (type: ProposalType) => {
-    const slug = type === ProposalType.poll ? "polling" : "executive"
-
-    const browser = await launchChromium({
-        headless: true, // setting this to true will not run the UI
-    })
-    const context = await browser.newContext()
-    const page = await context.newPage()
-    await page.goto(`https://vote.makerdao.com/${slug}`)
-    let text = await page.innerHTML("#__NEXT_DATA__")
-    await browser.close()
-
-    const entries = JSON.parse(text).props.pageProps[slug === "polling" ? "polls" : "proposals"]
-    const data =
-        slug === "polling"
-            ? entries.map((p: any) => {
-                  return {
-                      title: p.title,
-                      type: "Poll",
-                      voteType: p.parameters.inputFormat.type
-                          .split("-")
-                          .map((word: string[]) => word[0].toUpperCase() + String(word).substring(1))
-                          .join(" "),
-                      options: Object.values(p.options),
-                      dateAdded: p.startDate,
-                      dateExpiry: p.endDate,
-                      voteUrl: `https://vote.makerdao.com/${slug}/${p.slug}`,
-                      forumUrl: p.discussionLink,
-                      status: Status.Unassigned,
-                  }
-              })
-            : entries.map((p: any) => {
-                  return {
-                      title: p.title,
-                      type: "Executive Proposal",
-                      voteType: "Executive Proposal",
-                      options: [],
-                      dateAdded: p.spellData.datePassed,
-                      dateExpiry: p.spellData.expiration,
-                      dateExecuted: p.spellData.dateExecuted,
-                      voteUrl: `https://vote.makerdao.com/${slug}/${"template-executive-vote-"}${String(p.title)
-                          .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
-                          .replace(/ +(?= )/g, "")
-                          .toLowerCase()
-                          .replace(/ /g, "-")}${"#proposal-detail"}`,
-                      forumUrl: "",
-                      status: Status.Unassigned,
-                  }
-              })
-    return data
-}
-
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method !== "GET") {
         res.status(405).send({ message: "Only GET requests allowed" })
         return
+    }
+
+    const scrape = async (type: ProposalType) => {
+        const slug = type === ProposalType.poll ? "polling" : "executive"
+
+        const browser = await puppeteer.launch(
+            process.env.NODE_ENV === "production"
+                ? {
+                      args: chrome.args,
+                      executablePath: await chrome.executablePath,
+                      headless: chrome.headless,
+                  }
+                : {}
+        )
+
+        const page = await browser.newPage()
+        page.setUserAgent(
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"
+        )
+        await page.goto(`https://vote.makerdao.com/${slug}`)
+        const element = await page.waitForSelector("#__NEXT_DATA__")
+        const text = await page.evaluate((element: any) => element?.textContent, element)
+
+        browser.close()
+
+        if (!text) return []
+
+        const entries = JSON.parse(text).props.pageProps[slug === "polling" ? "polls" : "proposals"]
+        const data =
+            slug === "polling"
+                ? entries.map((p: any) => {
+                      return {
+                          title: p.title,
+                          type: "Poll",
+                          voteType: p.parameters.inputFormat.type
+                              .split("-")
+                              .map((word: string[]) => word[0].toUpperCase() + String(word).substring(1))
+                              .join(" "),
+                          options: Object.values(p.options),
+                          dateAdded: p.startDate,
+                          dateExpiry: p.endDate,
+                          voteUrl: `https://vote.makerdao.com/${slug}/${p.slug}`,
+                          forumUrl: p.discussionLink,
+                          status: Status.Unassigned,
+                      }
+                  })
+                : entries.map((p: any) => {
+                      return {
+                          title: p.title,
+                          type: "Executive Proposal",
+                          voteType: "Executive Proposal",
+                          options: [],
+                          dateAdded: p.spellData.datePassed,
+                          dateExpiry: p.spellData.expiration,
+                          dateExecuted: p.spellData.dateExecuted,
+                          voteUrl: `https://vote.makerdao.com/${slug}/${"template-executive-vote-"}${String(p.title)
+                              .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+                              .replace(/ +(?= )/g, "")
+                              .toLowerCase()
+                              .replace(/ /g, "-")}${"#proposal-detail"}`,
+                          forumUrl: "",
+                          status: Status.Unassigned,
+                      }
+                  })
+        return data
     }
 
     const polls = await scrape(ProposalType.poll)
